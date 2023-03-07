991f41e2c3b1ead1daeb1b7d75f5f4126f169ea4
r1chwo0n <woonzy09@gmail.com>
2023-03-07 17:00:44 +0700

    completed

--- lab24.cpp
+++ lab24.cpp
@@ -28,7 +28,64 @@ ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 	return ComplexNumber(real-c.real,imag-c.imag);
 }
 
-//Write your code here
+ComplexNumber ComplexNumber::operator*(const ComplexNumber &c){
+	return ComplexNumber((real*c.real)-(imag*c.imag),(imag*c.real)+(real*c.imag));
+}
+
+ComplexNumber ComplexNumber::operator/(const ComplexNumber &c){
+    double x = pow(c.real,2)+pow(c.imag,2);
+	return ComplexNumber(((real*c.real)+(imag*c.imag))/x,((imag*c.real)-(real*c.imag))/x);
+}
+
+ComplexNumber operator+(double s,const ComplexNumber &c){
+	return ComplexNumber(s+c.real,c.imag);
+}
+
+ComplexNumber operator-(double s,const ComplexNumber &c){
+	return ComplexNumber(s-c.real,-c.imag);
+}
+ComplexNumber operator*(double s,const ComplexNumber &c){
+	return ComplexNumber(s*c.real,s*c.imag);
+}
+ComplexNumber operator/(double s,const ComplexNumber &c){
+	return ComplexNumber((s*c.real)/(pow(c.real,2)+pow(c.imag,2)),(-(s*c.imag))/(pow(c.real,2)+pow(c.imag,2)));
+}
+
+ostream & operator<<(ostream &os, const ComplexNumber &c){
+    if (c.real == 0){
+        if(c.imag == 0) return os << "0";
+        else return os << c.imag << "i";
+    }
+    else if(c.imag > 0) return os << c.real << "+" << c.imag << "i";
+    else if(c.imag < 0) return os << c.real << c.imag << "i";
+    else return os << c.real;
+}
+
+bool ComplexNumber::operator==(const ComplexNumber &c){
+    if(real == c.real && imag == c.imag){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+bool operator==(double s,const ComplexNumber &c){
+    if(s == c.real && c.imag == 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+double ComplexNumber::abs(){
+    return sqrt(pow(real,2)+pow(imag,2));
+}
+
+double ComplexNumber::angle(){
+    return atan2(imag,real) * (180/M_PI) ;
+}
+
+
 
 int main(){
 	ComplexNumber a(1.5,2),b(3.2,-2.5),c(-1,1.2);	
@@ -69,9 +126,7 @@ int main(){
 	cout << (ComplexNumber(1,1) == ComplexNumber(1,2)) << "\n";
 	cout << (ComplexNumber(1,1) == 1) << "\n";
 	cout << (0 == ComplexNumber()) << "\n";
+
+    return 0;
 }
-
-
-
-
 
